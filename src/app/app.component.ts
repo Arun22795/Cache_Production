1a6c504f1f8c963acedd150f6db7700a62cdfa93
arunkumar <arunkumar.ravi@appiyo.com>
2021-01-12 11:26:39 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lazy-demo';
+
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private cfr: ComponentFactoryResolver
+  ){
+
+  }
+
+
+  async getLazy1() {
+    this.viewContainerRef.clear();
+    const {Lazy1Component} = await import('./lazy1/lazy1.component');
+    this.viewContainerRef.createComponent(
+      this.cfr.resolveComponentFactory(Lazy1Component)
+    );
+  }
+
+  async getLazy2() {
+    this.viewContainerRef.clear();
+    const { Lazy2Component } = await import('./lazy2/lazy2.component');
+    this.viewContainerRef.createComponent(
+      this.cfr.resolveComponentFactory(Lazy2Component)
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Lazy1Component } from './lazy1/lazy1.component';
+import { Lazy2Component } from './lazy2/lazy2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lazy1Component,
+    Lazy2Component
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+  entryComponents:[Lazy1Component,Lazy2Component],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
