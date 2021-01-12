c096272cdb82dd55aa2e64d42e26e320928541b5
arunkumar <arunkumar.ravi@appiyo.com>
2021-01-12 14:33:57 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Lazy1Component } from './lazy1/lazy1.component';
 import { Lazy2Component } from './lazy2/lazy2.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { Lazy2Component } from './lazy2/lazy2.component';
     Lazy2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   entryComponents:[Lazy1Component,Lazy2Component],
